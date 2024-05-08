8fa504512ad7b64daba9a41171546c3bef3805a6
TeDeLimon <tysonpopluis@gmail.com>
2024-05-08 13:46:23 +0200

    Includes, returns

---
+++ layouts/layout.php
@@ -0,0 +1,7 @@
+<?php include_once './../components/header.php'; 
+
+include_once './../components/header.php'; 
+
+require __DIR__ . '../components/body.php'; 
+
+include '../components/footer.php'; 
