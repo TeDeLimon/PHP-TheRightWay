6b222fde4a11775d83f52fee5f3b225727c1e399
TeDeLimon <tysonpopluis@gmail.com>
2024-05-08 16:49:32 +0200

    Reestructurando carpetas

--- layouts/layout.php
+++
@@ -1,7 +0,0 @@
-<?php include_once './../components/header.php'; 
-
-include_once './../components/header.php'; 
-
-require __DIR__ . '../components/body.php'; 
-
-include '../components/footer.php'; 
